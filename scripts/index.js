148ce4e524acbc565c5de35ee1459e4f70758f1e
Ilya Malyshev <malyshev-ilya-2002@yandex.ru>
2024-11-29 00:41:32 +0300

    mesto 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template')
 
-// @todo: DOM узлы
+// DOM узел для списка карточек
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(cardData, deleteCardCallback) {
+    const cardElement = cardTemplate.content.querySelector('.card').cloneNode(true);
+  
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteIcon = cardElement.querySelector('.card__delete-button');
+  
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+  
+    deleteIcon.addEventListener('click', () => deleteCardCallback(cardElement));
+  
+    return cardElement;
+  }
 
-// @todo: Функция удаления карточки
+// Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
 
-// @todo: Вывести карточки на страницу
+// Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.appendChild(cardElement);
+});
